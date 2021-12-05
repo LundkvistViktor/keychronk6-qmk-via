1909a51047b23fe8a4b1abb2ca3edbef7df0b58e
Viktor Lundkvist <lundkvist.viktor@gmail.com>
2021-12-05 05:26:28 +0100

    -

--- keyboards/keychron/k6/rgb/keymaps/virrjet/config_led.c
+++ keyboards/keychron/k6/rgb/keymaps/virrjet/config_led.c
@@ -37,14 +37,12 @@ led_config_t g_led_config = { {
    4, 4, 4,          4,          4, 4, 4, 4, 4, 4
 } };
 
-// Make keyboard red when capslock is enabled
+// Make caps key red when capslock is enabled, (should probably use led_update_user)
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
-        for (uint8_t i = led_min; i <= led_max; i++) {
-            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-                rgb_matrix_set_color(i, RGB_RED);
+            if (g_led_config.flags[29] & LED_FLAG_KEYLIGHT) {
+                rgb_matrix_set_color(29, RGB_RED);
             }
-        }
     }
 }
 
